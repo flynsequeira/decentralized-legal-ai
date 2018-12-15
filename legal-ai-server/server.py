846179294645f82add616cedbf7c491796c04326
hiteshproy@gmail.com <hiteshproy@gmail.com>
2018-12-16 03:09:45 +0530

    added casenpost API

--- legal-ai-server/server.py
+++ legal-ai-server/server.py
@@ -1,9 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, session
+from pymongo import MongoClient
+from elasticsearch import Elasticsearch
+import bcrypt
+import datetime
+import requests
+import json
+import time
 
-@app.route("/")
+def connect():
+    connection = MongoClient('localhost', 27017)
+    handle = connection["flask_reminders"]
+    return handle
+
+app = Flask(__name__)
+app.config['ELASTICSEARCH_URL'] = 'localhost:9200/'
+es =  Elasticsearch([app.config['ELASTICSEARCH_URL']])
+handle = connect()
+
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
+
+@app.route('/case', methods=['POST'])
+def data():
+    key1 = request.get_json()
+    print(key1['data'])
+
+    return  "{status:error, data: 'test'}"
+
+
+if __name__ == '__main__':
     app.run()

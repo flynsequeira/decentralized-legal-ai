121402ec74b2e4392746d33a5b328dff445261a0
hiteshproy@gmail.com <hiteshproy@gmail.com>
2018-12-16 13:17:07 +0530

    hide and show text of summary of case

--- webApp/src/app/layout/dashboard/dashboard.component.ts
+++ webApp/src/app/layout/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { RestService } from '../../shared/services/rest.service';
 import { DashboardService } from './dashboard.service';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { SHARED_FORM_DIRECTIVES } from '@angular/forms/src/directives';
 
 @Component({
     selector: 'app-dashboard',
@@ -15,26 +16,27 @@ export class DashboardComponent implements OnInit {
     showTable = false;
     error = false;
     emptyErr = false;
+    show = false;
 
-    demoDataset = {
-        "sentiment": {
-            "document": {
-             "score": -0.479108,
-             "label": "negative"
-            }
-        },
-        "emotion": {
-            "document": {
-                "emotion": {
-                    "sadness": 0.494381,
-                    "joy": 0.475491,
-                    "fear": 0.088296,
-                    "disgust": 0.127116,
-                    "anger": 0.133539
-                }
-            }
-        }
-    };
+    // demoDataset = {
+    //     "sentiment": {
+    //         "document": {
+    //          "score": -0.479108,
+    //          "label": "negative"
+    //         }
+    //     },
+    //     "emotion": {
+    //         "document": {
+    //             "emotion": {
+    //                 "sadness": 0.494381,
+    //                 "joy": 0.475491,
+    //                 "fear": 0.088296,
+    //                 "disgust": 0.127116,
+    //                 "anger": 0.133539
+    //             }
+    //         }
+    //     }
+    // };
 
 
     constructor(private _rest: RestService, private dashboardService: DashboardService, private _spinner: NgxSpinnerService) {
@@ -55,7 +57,8 @@ export class DashboardComponent implements OnInit {
 
             this.dashboardService.searchQuery(this.userQuery).subscribe(searchResult => {
                 this._spinner.hide();
-                this.caseList = searchResult['hits']['hits'];
+                this.caseList = searchResult['summarized_search_result'];
+                console.log(this.caseList);
                 this.showTable = true;
                 if (Object.keys(this.caseList).length === 0) {
                     this.showTable = false;
@@ -70,6 +73,18 @@ export class DashboardComponent implements OnInit {
             this._spinner.hide();
             this.emptyErr = true;
         }
+
+    }
+
+    toggleShow(i) {
+        console.log(i);
+       // document.querySelector('div-' + i).style.cssText = "--my-var: #000"
+        if ( document.getElementById(i).className.indexOf('show') > -1 ) {
+            document.getElementById(i).classList.remove('show');
+        } else {
+            document.getElementById(i).classList.add('show');
+        }
+        this.show = true;
 
     }
 
